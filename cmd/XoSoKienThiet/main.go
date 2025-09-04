e972155012d2dfdfc7ddcfdedf4610fecc8d44bf
dopaemon <polarisdp@gmail.com>
2025-09-04 14:25:17 +0700

    XSKT: Reduce folder Struct
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- cmd/XoSoKienThiet/main.go
+++ cmd/XoSoKienThiet/main.go
@@ -14,57 +14,67 @@ import (
 func main() {
 	fmt.Println(utils.Banner())
 
-	var province string
-	var wdate string
+	province := ""
+	wdate := ""
 
-	options := make([]huh.Option[string], len(configs.Provinces))
+	provinceOptions := make([]huh.Option[string], len(configs.Provinces))
 	for i, code := range configs.Provinces {
-		options[i] = huh.NewOption(code, code)
+		provinceOptions[i] = huh.NewOption(code, code)
 	}
 
-	selected := huh.NewSelect[string]().
-	Title("Chọn loại vé số bạn muốn: ").
-	Options(options...).
-	Value(&province)
+	provinceSelect := huh.NewSelect[string]().
+		Title("Chọn loại vé số:").
+		Options(provinceOptions...).
+		Value(&province)
 
-	huh.NewForm(huh.NewGroup(selected)).Run()
+	huh.NewForm(huh.NewGroup(provinceSelect)).Run()
 
 	url := rss.Sources(province)
-	data, _ := rss.Fetch(url)
-	results, _ := rss.Parse(data)
-
-	for _, r := range results {
-		configs.DateXS = append(configs.DateXS, r.Date)
+	data, err := rss.Fetch(url)
+	if err != nil {
+		fmt.Println("Lỗi fetch RSS:", err)
+		os.Exit(1)
 	}
 
-	for _, r := range results {
-		input := huh.NewInput().
-			Title("Bạn muốn xem kết quả sổ số ngày nào ?").
-			Prompt(": ").
-			Suggestions(configs.DateXS).
-			Value(&wdate)
-		huh.NewForm(huh.NewGroup(input)).Run()
+	results, err := rss.Parse(data)
+	if err != nil {
+		fmt.Println("Lỗi parse RSS:", err)
+		os.Exit(1)
+	}
 
-		if wdate == r.Date {
+	dateOptions := make([]huh.Option[string], len(results))
+	for i, r := range results {
+		dateOptions[i] = huh.NewOption(r.Date, r.Date)
+	}
+
+	dateSelect := huh.NewSelect[string]().
+		Title("Chọn ngày:").
+		Options(dateOptions...).
+		Value(&wdate)
+
+	huh.NewForm(huh.NewGroup(dateSelect)).Run()
+
+	found := false
+	for _, r := range results {
+		if r.Date == wdate {
 			fmt.Println("=== ", r.Title, " ===")
 			for giai, so := range r.Prizes {
 				fmt.Println("Giải", giai+":", so)
 			}
+			found = true
 			break
-		} else {
-			fmt.Println("!!! Không có kết quả cho ngày này !!!")
 		}
-		fmt.Println()
+	}
+	if !found {
+		fmt.Println("!!! Không có kết quả cho ngày này !!!")
 	}
 
 	switch utils.GenFlags() {
-		case "gui":
-			fmt.Println("Run GUI")
-			break
-		case "cli":
-			fmt.Println("Run CLI")
-			break
-		default:
-			os.Exit(1)
+	case "gui":
+		fmt.Println("Run GUI")
+	case "cli":
+		fmt.Println("Run CLI")
+	default:
+		os.Exit(1)
 	}
 }
