baa31a526b46578b185ad5e0f53b1067e309eacd
dopaemon <polarisdp@gmail.com>
2025-09-10 19:21:58 +0700

    KQXS: Fix derp typo
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- internal/configs/configs.go
+++ internal/configs/configs.go
@@ -17,9 +17,9 @@ var (
 	AppConfig Config
 
 	VERSION	string = "v0.0.1"
-	Provinces = []string{"Miền Bắc", "An Giang", "Bình Dương", "Bình Định", "Bạc Liêu", "Bình Phước", "Bến Tre", "Bình Thuận", "Cà Mau", "Cần Thơ", "Đắk Lắk", "Đồng Nai", "Đà Nẵng", "Đắk Nông", "Đồng Tháp", "Gia Lai", "Hồ Chí Minh", "Hậu Giang", "Kiên Giang", "Khánh Hòa", "Kon Tum", "Long An", "Lâm Đồng", "Ninh Thuận", "Phú Yên", "Quảng Bình", "Quảng Ngãi", "Quảng Nam", "Quảng Trị", "Sóc Trăng", "Tiền Giang", "Tây Ninh", "Thừa Thiên Huế", "Trà Vinh", "Vĩnh Long", "Vũng Tàu"}
-	DateXS = []string{} = []string{}
-	Order = []string{"ĐB", "1", "2", "3", "4", "5", "6", "7", "8"}
+	Provinces []string = []string{"Miền Bắc", "An Giang", "Bình Dương", "Bình Định", "Bạc Liêu", "Bình Phước", "Bến Tre", "Bình Thuận", "Cà Mau", "Cần Thơ", "Đắk Lắk", "Đồng Nai", "Đà Nẵng", "Đắk Nông", "Đồng Tháp", "Gia Lai", "Hồ Chí Minh", "Hậu Giang", "Kiên Giang", "Khánh Hòa", "Kon Tum", "Long An", "Lâm Đồng", "Ninh Thuận", "Phú Yên", "Quảng Bình", "Quảng Ngãi", "Quảng Nam", "Quảng Trị", "Sóc Trăng", "Tiền Giang", "Tây Ninh", "Thừa Thiên Huế", "Trà Vinh", "Vĩnh Long", "Vũng Tàu"}
+	DateXS []string = []string{}
+	Order []string = []string{"ĐB", "1", "2", "3", "4", "5", "6", "7", "8"}
 	Port string = "8080"
 	Origins string = "*"
 )
