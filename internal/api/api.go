5cc79375f06d9ce5bb49c75de64b8c37a5522bcc
dopaemon <polarisdp@gmail.com>
2025-09-10 16:05:39 +0700

    KQXS: README: Replace href `swagger`
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- internal/api/api.go
+++ internal/api/api.go
@@ -171,7 +171,7 @@ func RunAPI() {
 	r.POST("/api/check", CheckLottery)
 	r.POST("/api/check-ticket", CheckTicket)
 
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler)) /** **/
+	r.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler)) /** **/
 
 	r.Run(":" + configs.Port)
 }
